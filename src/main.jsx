f8517e6480df3d1bae73b23f8574fc8b6657317b
sharon12307 <sharon12307@gmail.com>
2024-12-17 13:25:43 +0800

    1217-7

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './components/css/style.css';
 
-// const basename = process.env.NODE_ENV === 'production' ? '/bandMapProject2/' : './';
+const basename = process.env.NODE_ENV === 'production' ? '/bandMapProject2/' : './';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
